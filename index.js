ab557520f8e449bd0411fb68a1b7c001d0e29617
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-11-08 15:16:58 +0100

    feat .map

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const library = [
         year: 2001,
         genre: "Fantasy",
         rating: 8.8,
-        type: "film"
+        type: "movie"
     },
     {
         title: "Breaking Bad",
         year: 2008,
         genre: "Drama",
         rating: 9.5,
-        type: "serie tv",
+        type: "tvSeries",
         seasons: 5
     },
     {
@@ -19,14 +19,14 @@ const library = [
         year: 2008,
         genre: "Action",
         rating: 9.0,
-        type: "film"
+        type: "movie"
     },
     {
         title: "Game of Thrones",
         year: 2011,
         genre: "Fantasy",
         rating: 9.3,
-        type: "serie tv",
+        type: "tvSeries",
         seasons: 8
     },
 ]
@@ -40,6 +40,9 @@ class Movie {
         this.type = type;
     }
     toString() {
+        if (this.type == "movie") {
+            return "film";
+        }
         return `${this.title} è un ${this.type} di genere ${this.genre}. E' stato rilasciato nel ${this.year} ed ha un voto di ${this.rating}.`
     }
 }
@@ -50,12 +53,21 @@ class TvSeries extends Movie {
         this.seasons = seasons;
     }
     toString() {
+        if (this.type == "tvSeries") {
+            return "serie tv";
+        }
         return `${this.title} è una ${this.type} di genere ${this.genre}. La prima stagione è stata rilasciata nel ${year} ed in totale sono state prodotte ${this.seasons}. Ha un voto di ${this.rating}.`
     }
 }
 
-// const filteredArray = library.map(title: String, year: Number, genre: String, rating: Number, type: String)
+const filteredArray = library.map(function(e) {
+    if (e.type == "movie") {
+        return new Movie(e.title, e.year, e.genre, e.rating, e.type);
+    } else {
+        return new TvSeries(e.title, e.year, e.genre, e.rating, e.type, e.seasons)
+    }
+})
 
-// const filteredArray = library.map(function(e) {
-    
-// }
+console.log(filteredArray)
+const film = new Movie("azz","2000","commedia","8.5","film")
+console.log(film.toString())
